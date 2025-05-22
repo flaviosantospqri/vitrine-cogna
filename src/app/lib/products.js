523507294fb243f6145960281377df0b73e5128e
Flavio Santos <flavio.pasantos@outlook.com>
2025-05-22 17:38:23 -0300

    fix: getapi

---
+++ src/app/lib/products.js
@@ -0,0 +1,9 @@
+export const fetchAllData = async () => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
