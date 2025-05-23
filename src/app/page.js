7fc7bb2fe18a74c9799fdc70135809a44113fbce
Flavio Santos <flavio.pasantos@outlook.com>
2025-05-23 16:22:31 -0300

    style: adjust in banner style

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,45 @@
 import React from "react";
-import { Api } from "../service/api";
 import Head from "next/head";
 import Gallery from "../components/gallery";
+import style from "./page.module.css";
+import { Api } from "../service/api";
+
+export const metadata = {
+  title: "Cogna | Vitrine de Produtos",
+  description: "A maior rede de educação do Brasil | Produtos Martech",
+};
 
 const Home = async () => {
-  //Após o tratamento da API, no arquivo responsável pego o resultado aqui, desta forma, não me desfaço do SSG.
-  const products = await Api.getAll();
+  try {
+    const products = await Api.getAll();
 
-  //Dou uma resposta visual ao usuário
-  if (!products || products.length === 0) {
-    return <p>Os produtos ainda não foram carregados</p>;
+    if (!products || products.length === 0) {
+      return <p>Os produtos ainda não foram carregados.</p>;
+    }
+
+    return (
+      <>
+        {/* aqui está sendo utilizado o Head do next, para adicionar o title e description 
+         embora a versão mais recente do Next, aconselhe o uso de metadata exportável, como fiz na página de produto*/}
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+        </Head>
+
+        <div className={style.container}>
+          <section className={style.banner}>
+            <h1>Produtos Exclusivos da Martech By Cogna</h1>
+            <p>A melhor qualidade em vestuário escolar/universitário.</p>
+          </section>
+        </div>
+
+        <Gallery products={products} />
+      </>
+    );
+  } catch (error) {
+    console.error("Erro ao carregar produtos:", error);
+    return <p>Erro ao carregar os produtos.</p>;
   }
-  return (
-    //Uso de Next/head para melhor busca pelos Crawlers do Google otimizando o SEO
-    <>
-      <Head>
-        <title>Cogna | Vitrine de Produtos</title>
-        <meta
-          name="description"
-          content="A maior rede de educação do Brasil | Produtos Martech"
-        />
-      </Head>
-      <h1>Produtos Exclusivos da Martech By Cogna</h1>
-      <Gallery products={products} />
-    </>
-  );
 };
+
 export default Home;
