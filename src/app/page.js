1db3a264d39c15a2bd83a3e92e1e486720a7ec2c
Flavio Santos <flavio.pasantos@outlook.com>
2025-05-23 09:48:47 -0300

    seo: adjust in SEO for Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Api } from "./service/api";
+import Head from "next/head";
 
 const Home = async () => {
   const products = await Api.getAll();
   return (
-    <div>
+    <>
+      <Head>
+        <title>Cogna | Vitrine de Produtos</title>
+        <meta
+          mame="description"
+          content="A maior rede de educação do Brasil | Produtos Exclusivos"
+        />
+      </Head>
       <h1>{products[0].title}</h1>
-    </div>
+    </>
   );
 };
 export default Home;
